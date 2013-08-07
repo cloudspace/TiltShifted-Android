ce368e84ed27e9382d094f83a551e2d6f7460953
Michael Beattie <beattie@cloudspace.com>
2013-08-07 14:26:10 -0400

    Removed old code

--- TiltShifted/src/com/cloudspace/tilt/Twitter/TwitterPoster.java
+++ TiltShifted/src/com/cloudspace/tilt/Twitter/TwitterPoster.java
@@ -1,16 +1,8 @@
 package com.cloudspace.tilt.Twitter;
 
-import java.io.File;
-
-import com.cloudspace.tilt.ImageManip.ImageLoader;
-
-import twitter4j.StatusUpdate;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -36,42 +28,6 @@ public class TwitterPoster extends Activity {
 	protected void onResume() {
 		super.onResume();
 	}
-
-	public void sendTweet() {
-		// Begin sharing photo
-		Intent i = new Intent(
-        		Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        		startActivityForResult(i,0);
-	}
-
-	@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
- 		super.onActivityResult(requestCode, resultCode, data);
- 		// Get file path of image to be shared
- 		galleryImage = ImageLoader.loadFromGallery(galleryImage, data, this);
- 		Thread t = new Thread() {
-	        public void run() {
-	        	try{
-	        		// Post image to Twitter
-	        		Twitter twitter = TwitterUtils.twitter;
-	        		Uri uri = Uri.fromFile(new File(galleryImage));
-	        		File file = new File(uri.getPath());
-	                StatusUpdate status = new StatusUpdate("Created with #TiltShifted for Android by @Cloudspace!");
-	                status.setMedia(file);
-	                twitter.updateStatus(status);}
-	            catch(TwitterException e){
-	                try {
-						throw e;
-					} catch (TwitterException e1) {
-						e1.printStackTrace();
-					}
-	            }
-	        }
- 		}    ;
- 		t.start();
- 		/** End the activity and send user back to the main activity */
- 		finish();
-	}
 	
     private class TwitterLogin extends AsyncTask<Void, Integer, Void>{
 		
@@ -92,10 +48,6 @@ public class TwitterPoster extends Activity {
 				Intent i = new Intent(getApplicationContext(), PrepareRequestTokenActivity.class);
 				startActivity(i);
 				finish();
-			}
-			else{
-				// Update status
-				sendTweet();
 			}
 		}
     }
